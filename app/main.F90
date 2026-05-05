0e3794a2270cf34f6b2fe39b93b2570b0becc368
Jean-Christophe <loiseau.jc@gmail.com>
2026-05-05 15:15:43 +0200

    Conditional compilation for 2D and 3D Poisson.

--- app/main.F90
+++ app/main.F90
@@ -1,29 +1,47 @@
 program main
-   use omp_lib
+   use omp_lib, only: omp_get_wtime
    use stdlib_constants, only: pi => pi_dp
    use stdlib_linalg_constants, only: ilp, dp, lk
    use stdlib_math, only: linspace
    use stdlib_io_npy, only: save_npy
-   use Jacobi_Experiments
-   implicit none
-   integer(ilp), parameter :: nx = 512, ny = nx, maxiter = nx**2
-   real(dp), dimension(nx, ny) :: b
-   real(dp), dimension(nx, ny) :: u
-   real(dp), dimension(nx) :: x
-   real(dp), dimension(ny) :: y
+   use Jacobi_Experiments, only: textbook_solver, nocopy_solver, &
+                                 otf_norm_solver, doconcurrent_solver
+
+   implicit none(type, external)
+   integer(ilp), parameter :: n = 128, maxiter = n**2
+#if NDIM == 3
+   real(dp), allocatable :: b(:, :, :), u(:, :, :)
+#else
+   real(dp), allocatable :: b(:, :), u(:, :)
+#endif
+   real(dp), dimension(n) :: x, y, z
    integer(ilp) :: i, j, k
    real(dp) :: start_time, end_time
 
    !----- Initialize variables -----!
-   b = 0.0_dp; u = 0.0_dp
-   x = linspace(0, 1, nx); y = linspace(0, 1, ny)
-
+    print *, "---------------"
+#if NDIM == 3
+   print *, "RUNNING 3D CASE."
+   allocate(b(n, n, n), source=0.0_dp)
+   x = linspace(0, 1, n)
+   y = linspace(0, 1, n)
+   z = linspace(0, 1, n)
+   do concurrent(i=1:n, j=1:n, k=1:n)
+      b(i, j, k) = sin(2*pi*x(i))*sin(2*pi*y(j))*sin(2*pi*z(k))
+   end do
+#else
+    print *, "RUNNING 2D CASE."
+   allocate(b(n, n), source=0.0_dp)
+   x = linspace(0, 1, n)
+   y = linspace(0, 1, n)
    ! Create right-hand side vector.
-   do concurrent(i=1:nx, j=1:ny)
+   do concurrent(i=1:n, j=1:n)
       b(i, j) = sin(2*pi*x(i))*sin(2*pi*y(j))
    end do
-
-   call save_npy("rhs_vector.npy", b)
+#endif
+   print *, "  - Number of points per direction :", n
+   print *
+   b = b/norm2(b)
 
    !----------------------------------
    !-----     JACOBI SOLVERS     -----
@@ -34,27 +52,17 @@ program main
    u = textbook_solver(b, maxiter)
    end_time = omp_get_wtime()
    print *, "    - Time-to-solution     :", end_time - start_time
-   call save_npy("textbook_solution.npy", u)
 
    !> No-copy Jacobi solver.
    start_time = omp_get_wtime()
    u = nocopy_solver(b, maxiter)
    end_time = omp_get_wtime()
    print *, "    - Time-to-solution     :", end_time - start_time
-   call save_npy("flipflop_solution.npy", u)
-
-   !> On-the-fly Jacobi solver.
-   start_time = omp_get_wtime()
-   u = otf_norm_solver(b, maxiter)
-   end_time = omp_get_wtime()
-   print *, "    - Time-to-solution     :", end_time - start_time
-   call save_npy("flipflop_solution.npy", u)
 
    !> doconcurrent Jacobi solver.
    start_time = omp_get_wtime()
    u = doconcurrent_solver(b, maxiter)
    end_time = omp_get_wtime()
    print *, "    - Time-to-solution     :", end_time - start_time
-   call save_npy("doconcurrent_solution.npy", u)
 
 end program main
